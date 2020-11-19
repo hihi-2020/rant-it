a58b073fa3cb5287e5a4ee50f9d0313911af7b93
student <student@pop-os.localdomain>
2020-11-20 11:53:28 +1300

    tidz

---
+++ server/routes/rantRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+// const db = require('../db/fruits')
+
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+  db.getAllRants()
+    .then(results => {
+      res.json({ rants: results.map(rant => rant.title) })
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "You've been HACKED" })
+    })
+})
+
+
+router.get('/:author', (req, res) => {
+  db.getAuthor()
+    .then(results => {
+      res.json({ author: results.map(author => rant.author) })
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "You've been HACKED" })
+    })
+})
+
+
+module.exports = router
