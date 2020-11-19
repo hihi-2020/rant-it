21ff5f709381b078e9a6e37670c873bbf4835087
student <student@pop-os.localdomain>
2020-11-20 10:42:55 +1300

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const fruitRoutes = require('./routes/fruits')
+const rantsRoutes = require('./routes/rants')
 
 const server = express()
 
@@ -9,5 +10,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/rants', rantsRoutes)
+
 
 module.exports = server
