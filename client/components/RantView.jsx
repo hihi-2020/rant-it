b74b71154e4172a1fa905bc8e428f5459f69dd3b
student <student@eda-bc-07.localdomain>
2020-11-20 11:38:28 +1300

    I see no changes, wake up in the morning and i ask myself

--- client/components/RantView.jsx
+++ client/components/RantView.jsx
@@ -1,22 +1,32 @@
 // Buddah_Chicken & DeathEagle
 import React from 'react'
+// import {getAllRantsAPI} from '../apis/rants'
+import rants from '../../server/db/seeds/rants'
+import { connect } from 'react-redux'
+
 
 
 
 const RantView = (props) => {
+  const rant = props.rant
+  //   state = {
+  //     rant:'',
+  //     author:'',
+  // }
+
 
   return (
     <div>
 
-      <li>
-      {this.props.rant.map(rant => (
+      <ul>
+      {rant.map(rant => (
             <li key={rant}>{rant}</li>
           ))}
-      </li>
+      </ul>
     </div>
 
   )
 }
 
-
+// reducer with empty state [], map state to props with 
 export default RantView
