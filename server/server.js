138b65e4f3261a291675332883b99cad42bc1bc4
student <student@pop-os.localdomain>
2020-11-20 11:16:08 +1300

    merge latest master

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
+// const fruitRoutes = require('./routes/fruits')
 const rantsRoutes = require('./routes/rants')
 const rantRoutes = require ('./routes/rants')
 
@@ -10,7 +10,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+// server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/rants', rantsRoutes)
 
 
