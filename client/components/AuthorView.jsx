a67b18a315b5fad7ce7e3daccb05a2d46cb1726e
jessicaveng <jessicaveng@outlook.com>
2020-11-21 23:05:50 +1300

    styling completed

--- client/components/AuthorView.jsx
+++ client/components/AuthorView.jsx
@@ -1,41 +1,42 @@
 // Buddah_Chicken & DeathEagle
-import React from 'react'
-import {connect} from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import rants from '../../server/db/seeds/rants'
-
+import rants from "../../server/db/seeds/rants";
 
 const AuthorView = (props) => {
-
   // const {rant} = rant
 
   return (
     <div>
-      <img src="./RANT-IT_DARK (1).svg"/>
-    <Link to={'/'}>Home!</Link>
-      <h1>Rants by: {props.match.params.author}</h1>
+      <img src="./RANT-IT_DARK (1).svg" />
+      <div className="homeLink">
+        <Link to={"/"}>Home</Link>
+      </div>
+      <div className="homeAllRantPosts">
+        <h1 id="rantAuthor">Rants by: {props.match.params.author}</h1>
 
-      {props.rants.filter(rant => rant.author==props.match.params.author).map((rant) => {
-        return(
-          <div key={rant.id}>
-          <h3>{rant.title}</h3>
-          <p>{rant.rant}</p>
-        </div>
-        )
-      })}
-     
-
+        {props.rants
+          .filter((rant) => rant.author == props.match.params.author)
+          .map((rant) => {
+            return (
+              <div key={rant.id}>
+                <h3>{rant.title}</h3>
+                <p className="singleRant">{rant.rant}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
-
-  )
-}
+  );
+};
 
 function mapStateToProps(globalState) {
-	return {
-		userName: globalState.userName,
-		rants: globalState.home,
-	};
+  return {
+    userName: globalState.userName,
+    rants: globalState.home,
+  };
 }
 
 export default connect(mapStateToProps)(AuthorView);
