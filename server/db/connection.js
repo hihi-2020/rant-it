632e0fb252ffd4d6a8cd390c84418796fdbca62a
Jessica Veng <jessicaveng@outlook.com>
2020-11-23 11:33:50 +1300

    fixed environment error

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,5 @@
 const knex = require('knex')
+const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
