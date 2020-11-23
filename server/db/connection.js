720ff7e6cf2ebfd20a64bd693425597d3917c7f5
Jessica Veng <jessicaveng@outlook.com>
2020-11-24 08:52:42 +1300

    more heroku stuff

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const knex = require('knex')
 const env = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[env]
+const config = require('./knexfile')
 const connection = knex(config[env])
 
 module.exports = connection
--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    const envConfig = require('dotenv').config()
-    if (envConfig.error) throw envConfig.error
-  }
+  // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  //   const envConfig = require('dotenv').config()
+  //   if (envConfig.error) throw envConfig.error
+  // }
   console.log('Server listening on port', PORT)
 })
